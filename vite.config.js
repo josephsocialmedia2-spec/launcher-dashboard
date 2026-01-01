5b35ffd14f004a69350049ce650c55c57ed03ee7
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-01-01 09:45:54 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Works well for GitHub Pages when deployed under a repo subpath.
-// If you deploy at the root domain, you can remove/adjust `base`.
-export default defineConfig({
-  plugins: [react()],
-  base: "./"
-});
