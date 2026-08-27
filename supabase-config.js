3bd7a700d07db3ea4051c970edbd98adb592320f
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-08-27 21:34:18 +0200

    f1: config cloud contatti e visite

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 window.F1_SUPABASE = {
   url: localStorage.getItem('f1SupabaseUrl') || '',
   anonKey: localStorage.getItem('f1SupabaseAnonKey') || '',
-  table: 'contacts'
+  table: 'contacts',
+  visitsTable: 'field_visits'
 };
