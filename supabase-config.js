43a343c8835805f25630149a3a7f1ce08a67993e
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-09-01 17:33:49 +0200

    feat: preconfigure F1 Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.F1_SUPABASE = {
-  url: localStorage.getItem('f1SupabaseUrl') || '',
-  anonKey: localStorage.getItem('f1SupabaseAnonKey') || '',
+  url: localStorage.getItem('f1SupabaseUrl') || 'https://nqnmlsmeiynxbdojeyjt.supabase.co',
+  anonKey: localStorage.getItem('f1SupabaseAnonKey') || 'sb_publishable_Clz5qPTkTtvwV0rqWTcfMQ_sCDSRgnu',
   table: 'contacts',
   visitsTable: 'field_visits'
 };
