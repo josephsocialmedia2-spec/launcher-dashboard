ed37e43b0faa74b5af605b9ee64ff42db42c169e
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-09-19 08:26:41 +0200

    Refresh F1 Script Trainer offline cache

--- mike-ferry-script-trainer/service-worker.js
+++ mike-ferry-script-trainer/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='f1-script-trainer-v1';
+const CACHE='f1-script-trainer-v2';
 const CORE=['./','./index.html','./app.js','./data/scripts.js','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
